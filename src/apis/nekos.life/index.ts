b2edc96fa7bc8562067ed4625ff62221a6e0de77
Mednoob <ahmadyaqdhan123@gmail.com>
2022-07-11 15:19:18 +0900

    feat(apis/nekos.life): make v2 the default export

--- src/apis/nekos.life/index.ts
+++ src/apis/nekos.life/index.ts
@@ -1,2 +1,11 @@
-export * as v1 from "./v1.js";
-export * as v2 from "./v2.js";
+import * as v1 from "./v1.js";
+import * as v2 from "./v2.js";
+
+export * from "./v2.js";
+
+export { v1, v2 };
+export default {
+    ...v2,
+    v2,
+    v1
+};
